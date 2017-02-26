1dec3437b76291ced7f820c7fe0680c1a869cf16
tinmarino <tinmarino@gmail.com>
2017-02-26 20:38:43 +0100

    Remove PixmapIO for html (I used it for screenshots) + uploaded a new Page on Github pages. TODO make html screen good dimention

--- core/src/com/mygdx/game/G.java
+++ core/src/com/mygdx/game/G.java
@@ -3,7 +3,8 @@ package com.mygdx.game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Pixmap;
-import com.badlogic.gdx.graphics.PixmapIO;
+// not html
+// import com.badlogic.gdx.graphics.PixmapIO;
 import com.badlogic.gdx.utils.BufferUtils;
 import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.TimeUtils;
@@ -21,7 +22,7 @@ public class G{
 		byte[] pixels = ScreenUtils.getFrameBufferPixels(0, 0, Gdx.graphics.getBackBufferWidth(), Gdx.graphics.getBackBufferHeight(), true);
 		Pixmap pixmap = new Pixmap(Gdx.graphics.getBackBufferWidth(), Gdx.graphics.getBackBufferHeight(), Pixmap.Format.RGBA8888);
 		BufferUtils.copy(pixels, 0, pixmap.getPixels(), pixels.length);
-		PixmapIO.writePNG(Gdx.files.external("screenshot" + TimeUtils.millis() + ".png"), pixmap);
+		// PixmapIO.writePNG(Gdx.files.external("screenshot" + TimeUtils.millis() + ".png"), pixmap);
 		pixmap.dispose();
 	}
 
